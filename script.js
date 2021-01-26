0e143286db9c2421f4a4b3728e0985fe0e199ed7
Pranjal Chaplot <pranjalchaplot@gmail.com>
2021-01-26 14:20:23 +0530

    add Write User Speech and Check validity

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const randomNum = getRandomNumber();
 
 console.log('Number: ' + randomNum);
 
+// @ts-ignore
 var recognition = new webkitSpeechRecognition() || new SpeechRecognition();
 
 // Start Recogniton and Game
@@ -12,6 +13,26 @@ recognition.start();
 // Capture User Speak
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
+
+    writeMessage(msg);
+    checkNumber(msg);  
+}
+
+// Write User Speech
+function writeMessage(msg){
+    msgEl.innerHTML = `
+    <div> You Said: </div>
+    <span class = "box">${msg}</span>
+    `;
+}
+
+// Check The User Number
+function checkNumber(msg){
+    const num = +msg;
+    // Check if valid number
+    if(Number.isNaN(num)){
+        msgEl.innerHTML = `<div>That is not a number</div>`;
+    }
 }
 
 // Generate Random Number
